806ca5f726ef702a0352cdc7eb91947c2b485404
ju4tlearn <ju4tlearn@gmail.com>
2024-10-08 14:25:45 +0800

    :construction: 初始化项目结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'index',
+      component: () => import('@/views/IndexView.vue'),
+      children: [
+        {
+          path: '/repos',
+          name: 'repos',
+          component: () => import('@/views/ReposView.vue')
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue')
+    },
+    {
+      path: '/:pathMatched(.*)*',
+      name: 'Not Found',
+      props: (route: { params: { pathMatched: string } }) => ({
+        pathMatched: `/${route.params.pathMatched}`
+      }),
+      component: () => import('@/views/NotFound.vue')
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.getItem('isLogin') === '1'
+  if (to.name !== 'login' && !isLogin) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
