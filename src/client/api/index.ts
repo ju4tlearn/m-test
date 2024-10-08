2cbd7dab413a686d9f96c9230a2b6bc4e0eb6593
ju4tlearn <ju4tlearn@gmail.com>
2024-10-08 17:44:05 +0800

    :sparkles: 完善用户相关 API

--- src/client/api/index.ts
+++ src/client/api/index.ts
@@ -1,5 +1,5 @@
 import { useFetch } from '@vueuse/core'
-import type { LoginResponse } from './models'
+import type { GetUserInfoResponse, RepoInfo, LoginResponse } from './models'
 
 export class AuthService {
   public static async login(
@@ -19,6 +19,49 @@ export class AuthService {
         scope: scope
       })
       .json<LoginResponse>()
+
+    if (error.value) {
+      return error.value
+    }
+
+    return data.value
+  }
+}
+
+export class UserService {
+  public static async getUserInfo(accessToken: string) {
+    const { data, error } = await useFetch(
+      `https://gitee.com/api/v5/user?access_token=${accessToken}`
+    )
+      .get()
+      .json<GetUserInfoResponse>()
+
+    if (error.value) {
+      return error.value
+    }
+
+    return data.value
+  }
+
+  public static async getUserRepos(
+    accessToken: string,
+    visibility?: 'public' | 'private' | 'all',
+    affiliation?: string,
+    type?: string,
+    sort?: 'full_name' | 'created' | 'updated' | 'pushed',
+    q?: string,
+    page: number = 1,
+    per_page: number = 20
+  ) {
+    let url = 'https://gitee.com/api/v5/user/repos'
+    url += `?access_token=${accessToken}&page=${page}&per_page=${per_page}`
+    if (visibility) url += `&visibility=${visibility}`
+    if (affiliation) url += `&affiliation=${affiliation}`
+    if (type && !visibility) url += `&type=${type}`
+    if (sort) url += `&sort=${sort}`
+    if (q) url += `&q=${q}`
+
+    const { data, error } = await useFetch(url).get().json<RepoInfo[]>()
 
     if (error.value) {
       return error.value
